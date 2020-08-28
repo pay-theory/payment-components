a066e48742f9068b266b010bd92132b0167c2926
Ubuntu <ubuntu@ip-172-31-37-209.ec2.internal>
2020-08-28 16:04:57 +0000

    putting in source map

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'none',
+  devtool: 'eval-source-map',
   module: {
     rules: [{
       test: /\.m?js$/,
