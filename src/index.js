7e43c636ba7ef130252d329c5fec2bf165ef0e21
Ubuntu <ubuntu@ip-172-31-37-209.ec2.internal>
2020-09-02 19:58:17 +0000

    nearing 0.2

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import 'regenerator-runtime'
 import './style.css'
 
 import createPaymentFields from './field-set/payment-fields'
-import createCreditCardFields from './field-set/split'
-import createCreditCard from './field-set/combined'
 
 window.paytheory = {
-  createPaymentFields,
-  createCreditCardFields,
-  createCreditCard
+  createPaymentFields
 }
 
 export default window.paytheory
