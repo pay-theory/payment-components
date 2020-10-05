2cdcbc3b0a26cc217b3cf07afb587bf12899b2c8
Ubuntu <ubuntu@ip-172-31-37-209.ec2.internal>
2020-10-05 15:58:28 +0000

    removing sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: "production",
-  devtool: 'eval-source-map',
+  devtool: 'none',
   module: {
     rules: [{
       test: /\.m?js$/,
