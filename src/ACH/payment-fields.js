9a4199d9217a7ed7ff0513f72e5d128117962057
Austin Zani <austinzani@gmail.com>
2020-12-29 17:01:46 +0000

    updated post messages for iframe

--- src/ACH/payment-fields.js
+++ src/ACH/payment-fields.js
@@ -111,17 +111,16 @@ export default async(
     }
 
     const setupHandler = () => {
-        window.getElementsByTagName('iframe').forEach(element => {
-            console.log(element)
-            console.log(common.hostedFieldsEndpoint)
-            element.contentWindow.postMessage({
+        const length = document.getElementsByTagName('iframe').length
+        for (var i = 0; i < length; i++) {
+            document.getElementsByTagName('iframe')[i].contentWindow.postMessage({
                     type: "pt:setup",
                     style: styles,
                     token: { ready: true }
                 },
                 common.hostedFieldsEndpoint,
             );
-        })
+        }
     }
 
     common.handleHostedFieldMessage(common.hostedReadyTypeMessage, setupHandler)
