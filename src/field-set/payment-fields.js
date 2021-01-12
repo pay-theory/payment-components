f76a899ae32853777deafdcebf2b7a67d5cb8481
Austin Zani <austinzani@gmail.com>
2021-01-12 21:11:46 +0000

    updated init transaction and valid observer to handle if both ACH and Card fields are filled out and valid

--- src/field-set/payment-fields.js
+++ src/field-set/payment-fields.js
@@ -416,7 +416,6 @@ export default async(
                 validating = (validatingCard && validatingDetails && validAch) ? 'both' : (validatingCard && validatingDetails) ? 'card' : validAch ? 'ach' : false
 
                 if (isCallingType(type) && isValid !== validating) {
-                    console.log(validating)
                     isValid = validating
                     cb(isValid)
                 }
