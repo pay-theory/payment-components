dc0c4d98754406a4acf0056a244a4a59f84cae03
aron@paytheory.com <ubuntu@ip-172-31-37-209.ec2.internal>
2021-03-11 22:02:01 +0000

    working on complexity

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,39 +1,16 @@
 /*eslint no-extend-native: ["error", { "exceptions": ["String", "Array"] }]*/
 //https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+const endsIWithIsValid = (searchString, position) => (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length)
+const endsWithResult = (lastIndex, position) => (lastIndex !== -1 && lastIndex === position)
 if (!String.prototype.endsWith) {
     String.prototype.endsWith = function (searchString, position) {
         var subjectString = this.toString();
-        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        if (endsIWithIsValid(searchString, position)) {
             position = subjectString.length;
         }
         position -= searchString.length;
         var lastIndex = subjectString.indexOf(searchString, position);
-        return lastIndex !== -1 && lastIndex === position;
-    };
-}
-if (![].fill) {
-    Array.prototype.fill = function (value) {
-
-        var O = Object(this);
-        var len = parseInt(O.length, 10);
-        var start = arguments[1];
-        var relativeStart = parseInt(start, 10) || 0;
-        var k = relativeStart < 0 ?
-            Math.max(len + relativeStart, 0) :
-            Math.min(relativeStart, len);
-        var end = arguments[2];
-        var relativeEnd = end === undefined ?
-            len :
-            (parseInt(end, 10) || 0);
-        var final = relativeEnd < 0 ?
-            Math.max(len + relativeEnd, 0) :
-            Math.min(relativeEnd, len);
-
-        for (; k < final; k++) {
-            O[k] = value;
-        }
-
-        return O;
+        return endsWithResult(lastIndex, position)
     };
 }
 
