a21d6e35a074f1f4420bce8ac6f6e3b8c94b7a5c
aron@paytheory.com <ubuntu@ip-172-31-37-209.ec2.internal>
2021-03-12 14:12:27 +0000

    working on complexity

--- src/polyfill.js
+++ src/polyfill.js
@@ -289,108 +289,108 @@ const buildHeaders = (base, body) => {
     }
 }
 
-const blobFunc =
 
-    function Body() {
-        this.bodyUsed = false
 
-        this._initBody = function (body) {
-            /*
-              fetch-mock wraps the Response object in an ES6 Proxy to
-              provide useful test harness features such as flush. However, on
-              ES5 browsers without fetch or Proxy support pollyfills must be used;
-              the proxy-pollyfill is unable to proxy an attribute unless it exists
-              on the object before the Proxy is created. This change ensures
-              Response.bodyUsed exists on the instance, while maintaining the
-              semantic of setting Request.bodyUsed in the constructor before
-              _initBody is called.
-            */
-            this.bodyUsed = this.bodyUsed
-            this._bodyInit = body
-            buildBody(this, body)
+function Body() {
+    this.bodyUsed = false
 
-            if (!this.headers.get('content-type')) {
-                buildHeaders(this, body)
-            }
+    this._initBody = function (body) {
+        /*
+          fetch-mock wraps the Response object in an ES6 Proxy to
+          provide useful test harness features such as flush. However, on
+          ES5 browsers without fetch or Proxy support pollyfills must be used;
+          the proxy-pollyfill is unable to proxy an attribute unless it exists
+          on the object before the Proxy is created. This change ensures
+          Response.bodyUsed exists on the instance, while maintaining the
+          semantic of setting Request.bodyUsed in the constructor before
+          _initBody is called.
+        */
+        this.bodyUsed = this.bodyUsed
+        this._bodyInit = body
+        buildBody(this, body)
+
+        if (!this.headers.get('content-type')) {
+            buildHeaders(this, body)
         }
+    }
 
-        if (support.blob) {
-            this.blob = function () {
-                var rejected = consumed(this)
-                if (rejected) {
-                    return rejected
-                }
-
-                if (this._bodyBlob) {
-                    return Promise.resolve(this._bodyBlob)
-                }
-                else if (this._bodyArrayBuffer) {
-                    return Promise.resolve(new Blob([this._bodyArrayBuffer]))
-                }
-                else if (this._bodyFormData) {
-                    throw new Error('could not read FormData body as blob')
-                }
-                else {
-                    return Promise.resolve(new Blob([this._bodyText]))
-                }
-            }
-
-            this.arrayBuffer = function () {
-                if (this._bodyArrayBuffer) {
-                    var isConsumed = consumed(this)
-                    if (isConsumed) {
-                        return isConsumed
-                    }
-                    if (ArrayBuffer.isView(this._bodyArrayBuffer)) {
-                        return Promise.resolve(
-                            this._bodyArrayBuffer.buffer.slice(
-                                this._bodyArrayBuffer.byteOffset,
-                                this._bodyArrayBuffer.byteOffset + this._bodyArrayBuffer.byteLength
-                            )
-                        )
-                    }
-                    else {
-                        return Promise.resolve(this._bodyArrayBuffer)
-                    }
-                }
-                else {
-                    return this.blob().then(readBlobAsArrayBuffer)
-                }
-            }
-        }
-
-        this.text = function () {
+    if (support.blob) {
+        this.blob = function () {
             var rejected = consumed(this)
             if (rejected) {
                 return rejected
             }
 
             if (this._bodyBlob) {
-                return readBlobAsText(this._bodyBlob)
+                return Promise.resolve(this._bodyBlob)
             }
             else if (this._bodyArrayBuffer) {
-                return Promise.resolve(readArrayBufferAsText(this._bodyArrayBuffer))
+                return Promise.resolve(new Blob([this._bodyArrayBuffer]))
             }
             else if (this._bodyFormData) {
-                throw new Error('could not read FormData body as text')
+                throw new Error('could not read FormData body as blob')
             }
             else {
-                return Promise.resolve(this._bodyText)
+                return Promise.resolve(new Blob([this._bodyText]))
             }
         }
 
-        if (support.formData) {
-            this.formData = function () {
-                return this.text().then(decode)
+        this.arrayBuffer = function () {
+            if (this._bodyArrayBuffer) {
+                var isConsumed = consumed(this)
+                if (isConsumed) {
+                    return isConsumed
+                }
+                if (ArrayBuffer.isView(this._bodyArrayBuffer)) {
+                    return Promise.resolve(
+                        this._bodyArrayBuffer.buffer.slice(
+                            this._bodyArrayBuffer.byteOffset,
+                            this._bodyArrayBuffer.byteOffset + this._bodyArrayBuffer.byteLength
+                        )
+                    )
+                }
+                else {
+                    return Promise.resolve(this._bodyArrayBuffer)
+                }
+            }
+            else {
+                return this.blob().then(readBlobAsArrayBuffer)
             }
         }
-
-        this.json = function () {
-            return this.text().then(JSON.parse)
-        }
-
-        return this
     }
+
+    this.text = function () {
+        var rejected = consumed(this)
+        if (rejected) {
+            return rejected
+        }
+
+        if (this._bodyBlob) {
+            return readBlobAsText(this._bodyBlob)
+        }
+        else if (this._bodyArrayBuffer) {
+            return Promise.resolve(readArrayBufferAsText(this._bodyArrayBuffer))
+        }
+        else if (this._bodyFormData) {
+            throw new Error('could not read FormData body as text')
+        }
+        else {
+            return Promise.resolve(this._bodyText)
+        }
+    }
+
+    if (support.formData) {
+        this.formData = function () {
+            return this.text().then(decode)
+        }
+    }
+
+    this.json = function () {
+        return this.text().then(JSON.parse)
+    }
+
+    return this
+}
 
 // HTTP methods whose capitalization should be normalized
 var methods = ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT']
@@ -565,13 +565,99 @@ catch (err) {
     DOMException.prototype.constructor = DOMException
 }
 
+const isAbort = (request, reject) => {
+    if (request.signal && request.signal.aborted) reject(new DOMException('Aborted', 'AbortError'))
+}
+
+const xhrBuildOnLoad = (xhr, resolve) => () => {
+    var options = {
+        status: xhr.status,
+        statusText: xhr.statusText,
+        headers: parseHeaders(xhr.getAllResponseHeaders() || '')
+    }
+    options.url = 'responseURL' in xhr ? xhr.responseURL : options.headers.get('X-Request-URL')
+    var body = 'response' in xhr ? xhr.response : xhr.responseText
+    setTimeout(function () {
+        resolve(new Response(body, options))
+    }, 0)
+}
+
+const xhrBuildOnError = (reject) => () => {
+    setTimeout(function () {
+        reject(new TypeError('Network request failed'))
+    }, 0)
+}
+
+const xhrBuildOnTimeout = (reject) => () => {
+    setTimeout(function () {
+        reject(new TypeError('Network request failed'))
+    }, 0)
+}
+const xhrBuildOnAbort = (reject) => () => {
+    setTimeout(function () {
+        reject(new DOMException('Aborted', 'AbortError'))
+    }, 0)
+}
+const xhrBuildOnReadyStateChange = (request, abortXhr, xhr) => () {
+    // DONE (success or failure)
+    if (xhr.readyState === 4) {
+        request.signal.removeEventListener('abort', abortXhr)
+    }
+}
+
+const fixUrl = (url) => {
+    try {
+        return url === '' && global.location.href ? global.location.href : url
+    }
+    catch (e) {
+        return url
+    }
+}
+
+const findXhrCredentials = (request) => {
+    if (request.credentials === 'include') {
+        return true
+    }
+    else if (request.credentials === 'omit') {
+        return false
+    }
+    return
+}
+
+const findXhrResponseType = (request, xhr) => {
+    if ('responseType' in xhr) {
+        if (support.blob) {
+            return 'blob'
+        }
+        else if (
+            support.arrayBuffer &&
+            request.headers.get('Content-Type') &&
+            request.headers.get('Content-Type').indexOf('application/octet-stream') !== -1
+        ) {
+            return 'arraybuffer'
+        }
+    }
+    return
+}
+
+const findXhrRequestHeader = (init, request, xhr) => {
+    if (init && typeof init.headers === 'object' && !(init.headers instanceof Headers)) {
+        Object.getOwnPropertyNames(init.headers).forEach(function (name) {
+            xhr.setRequestHeader(name, normalizeValue(init.headers[name]))
+        })
+    }
+    else {
+        request.headers.forEach(function (value, name) {
+            xhr.setRequestHeader(name, value)
+        })
+    }
+}
+
 export function fetch(input, init) {
     return new Promise(function (resolve, reject) {
         var request = new Request(input, init)
 
-        if (request.signal && request.signal.aborted) {
-            reject(new DOMException('Aborted', 'AbortError'))
-        }
+        isAbort(request, reject)
 
         var xhr = new XMLHttpRequest()
 
@@ -579,88 +665,26 @@ export function fetch(input, init) {
             xhr.abort()
         }
 
-        xhr.onload = function () {
-            var options = {
-                status: xhr.status,
-                statusText: xhr.statusText,
-                headers: parseHeaders(xhr.getAllResponseHeaders() || '')
-            }
-            options.url = 'responseURL' in xhr ? xhr.responseURL : options.headers.get('X-Request-URL')
-            var body = 'response' in xhr ? xhr.response : xhr.responseText
-            setTimeout(function () {
-                resolve(new Response(body, options))
-            }, 0)
-        }
+        xhr.onload = xhrBuildOnLoad(xhr, resolve)
 
-        xhr.onerror = function () {
-            setTimeout(function () {
-                reject(new TypeError('Network request failed'))
-            }, 0)
-        }
+        xhr.onerror = xhrBuildOnError(reject)
 
-        xhr.ontimeout = function () {
-            setTimeout(function () {
-                reject(new TypeError('Network request failed'))
-            }, 0)
-        }
+        xhr.ontimeout = xhrBuildOnTimeout(reject)
 
-        xhr.onabort = function () {
-            setTimeout(function () {
-                reject(new DOMException('Aborted', 'AbortError'))
-            }, 0)
-        }
-
-        function fixUrl(url) {
-            try {
-                return url === '' && global.location.href ? global.location.href : url
-            }
-            catch (e) {
-                return url
-            }
-        }
+        xhr.onabort = xhrBuildOnAbort(reject)
 
         xhr.open(request.method, fixUrl(request.url), true)
 
-        if (request.credentials === 'include') {
-            xhr.withCredentials = true
-        }
-        else if (request.credentials === 'omit') {
-            xhr.withCredentials = false
-        }
+        xhr.withCredentials = findXhrCredentials(request)
 
-        if ('responseType' in xhr) {
-            if (support.blob) {
-                xhr.responseType = 'blob'
-            }
-            else if (
-                support.arrayBuffer &&
-                request.headers.get('Content-Type') &&
-                request.headers.get('Content-Type').indexOf('application/octet-stream') !== -1
-            ) {
-                xhr.responseType = 'arraybuffer'
-            }
-        }
+        xhr.responseType = findXhrResponseType()
 
-        if (init && typeof init.headers === 'object' && !(init.headers instanceof Headers)) {
-            Object.getOwnPropertyNames(init.headers).forEach(function (name) {
-                xhr.setRequestHeader(name, normalizeValue(init.headers[name]))
-            })
-        }
-        else {
-            request.headers.forEach(function (value, name) {
-                xhr.setRequestHeader(name, value)
-            })
-        }
+        findXhrRequestHeader(init, request, xhr)
 
         if (request.signal) {
             request.signal.addEventListener('abort', abortXhr)
 
-            xhr.onreadystatechange = function () {
-                // DONE (success or failure)
-                if (xhr.readyState === 4) {
-                    request.signal.removeEventListener('abort', abortXhr)
-                }
-            }
+            xhr.onreadystatechange = xhrBuildOnReadyStateChange(request, abortXhr, xhr)
         }
 
         xhr.send(typeof request._bodyInit === 'undefined' ? null : request._bodyInit)
