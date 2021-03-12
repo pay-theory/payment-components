b8f8b772dd87da5ceca746ff22025e24387ede29
aron@paytheory.com <ubuntu@ip-172-31-37-209.ec2.internal>
2021-03-12 14:38:14 +0000

    working on complexity

--- src/polyfill.js
+++ src/polyfill.js
@@ -400,6 +400,38 @@ function normalizeMethod(method) {
     return methods.indexOf(upcased) > -1 ? upcased : method
 }
 
+const buildRequestHeaders = () => {
+    if (!options.headers) {
+        return new Headers(input.headers)
+    }
+    return
+}
+
+const handleInputRequest = (base, options, input) => {
+    if (input.bodyUsed) {
+        throw new TypeError('Already read')
+    }
+    base.url = input.url
+    base.credentials = input.credentials
+    base.headers = buildRequestHeaders(base, options, input)
+    base.method = input.method
+    base.mode = input.mode
+    base.signal = input.signal
+    if (!body && input._bodyInit != null) {
+        body = input._bodyInit
+        input.bodyUsed = true
+    }
+}
+
+const isInputRequest = (base, options, input) => {
+    if (input instanceof Request) {
+        handleInputRequest(base, options, input)
+    }
+    else {
+        base.url = String(input)
+    }
+}
+
 export function Request(input, options) {
     if (!(this instanceof Request)) {
         throw new TypeError('Please use the "new" operator, this DOM object constructor cannot be called as a function.')
@@ -408,26 +440,7 @@ export function Request(input, options) {
     options = options || {}
     var body = options.body
 
-    if (input instanceof Request) {
-        if (input.bodyUsed) {
-            throw new TypeError('Already read')
-        }
-        this.url = input.url
-        this.credentials = input.credentials
-        if (!options.headers) {
-            this.headers = new Headers(input.headers)
-        }
-        this.method = input.method
-        this.mode = input.mode
-        this.signal = input.signal
-        if (!body && input._bodyInit != null) {
-            body = input._bodyInit
-            input.bodyUsed = true
-        }
-    }
-    else {
-        this.url = String(input)
-    }
+
 
     this.credentials = options.credentials || this.credentials || 'same-origin'
     if (options.headers || !this.headers) {
