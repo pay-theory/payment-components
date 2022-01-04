7a2726cdb56713c6904ee0f2f293f0b0597c4f3b
satishgampala <satish@paytheory.com>
2022-01-04 11:25:06 +0000

    Change in Git branches creation

--- shell/01_prepare.sh
+++ shell/01_prepare.sh
@@ -6,6 +6,7 @@ STAGE=$2
 local existed_in_remote=$(git ls-remote --heads origin ${PARTNER}-${STAGE})
 if [[ -z ${existed_in_remote} ]]
 then
+    echo "Branch ${PARTNER}-${STAGE} not existed in remote, Creating now..."
     git checkout -b ${PARTNER}-${STAGE}
     git push -u origin ${PARTNER}-${STAGE}
 fi
