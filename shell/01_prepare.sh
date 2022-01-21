16f47d031205bf38fa897c17ff500240424d33a9
Abel Collins <66232145+abelcollins@users.noreply.github.com>
2022-01-21 12:19:17 -0500

    fix 01_prepare script

--- shell/01_prepare.sh
+++ shell/01_prepare.sh
@@ -3,10 +3,20 @@
 PARTNER=$1
 STAGE=$2
 
-existed_in_remote=$(git ls-remote --heads origin ${PARTNER}${MODE}-${STAGE})
+# if there is a .git directory we are in partner-factory
+if [ -d ".git" ]
+then
+
+# if we do not have the branch in remote create it
+existed_in_remote=$(git ls-remote --heads origin ${PARTNER}${MODE}-${STAGE} 2>/dev/null)
 if [[ -z ${existed_in_remote} ]]
 then
     echo "Branch ${PARTNER}${MODE}-${STAGE} not existed in remote, Creating now..."
     git checkout -b ${PARTNER}${MODE}-${STAGE}
     git push -u origin ${PARTNER}${MODE}-${STAGE}
+
+#end if we do not have the branch in remote create it
+fi
+
+#end if there is a .git directory we are in partner-factory
 fi
