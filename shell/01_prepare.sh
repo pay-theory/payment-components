8c32884ba6f1a1559707ce3cf991f17bea14e1b8
Abel Collins <66232145+abelcollins@users.noreply.github.com>
2022-01-20 21:02:59 -0500

    update 01_prepare with MODE variable

--- shell/01_prepare.sh
+++ shell/01_prepare.sh
@@ -3,10 +3,10 @@
 PARTNER=$1
 STAGE=$2
 
-existed_in_remote=$(git ls-remote --heads origin ${PARTNER}-prototype-${STAGE})
+existed_in_remote=$(git ls-remote --heads origin ${PARTNER}${MODE}-${STAGE})
 if [[ -z ${existed_in_remote} ]]
 then
-    echo "Branch ${PARTNER}-prototype-${STAGE} not existed in remote, Creating now..."
-    git checkout -b ${PARTNER}-prototype-${STAGE}
-    git push -u origin ${PARTNER}-prototype-${STAGE}
+    echo "Branch ${PARTNER}${MODE}-${STAGE} not existed in remote, Creating now..."
+    git checkout -b ${PARTNER}${MODE}-${STAGE}
+    git push -u origin ${PARTNER}${MODE}-${STAGE}
 fi
