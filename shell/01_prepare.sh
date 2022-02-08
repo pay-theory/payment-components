c796daffd4041601343772927c4ebd6ea4734426
EC2 Default User <your@email.com>
2022-02-08 15:46:09 +0000

    bumping to update 01_prepare with branch mode

--- shell/01_prepare.sh
+++ shell/01_prepare.sh
@@ -2,19 +2,43 @@
 
 PARTNER=$1
 STAGE=$2
+SERVICE_TYPE=$3
+SERVICE_NAME=$4
+GITHUB_ACCESS_TOKEN=$5
+
+
+TARGET_MODE=$6
+
+if [ $TARGET_MODE == "standard" ]; 
+then
+    MODE=""
+else
+    MODE=$TARGET_MODE
+fi
 
 # if there is a .git directory we are in partner-factory
 if [ -d ".git" ]
 then
 
 # if we do not have the branch in remote create it
-existed_in_remote=$(git ls-remote --heads origin ${PARTNER}-${STAGE} 2>/dev/null)
+existed_in_remote=$(git ls-remote --heads origin ${PARTNER}${MODE}-${STAGE} 2>/dev/null)
 if [[ -z ${existed_in_remote} ]]
 then
-    echo "Branch ${PARTNER}-${STAGE} not existed in remote, Creating now..."
-    git checkout -b ${PARTNER}-${STAGE}
-    git push -u origin ${PARTNER}-${STAGE}
-
+    echo "Branch ${PARTNER}${MODE}-${STAGE} not existed in remote, Creating now..."
+    git checkout -b ${PARTNER}${MODE}-${STAGE}
+    git push -u origin ${PARTNER}${MODE}-${STAGE}
+else
+    echo "Branch ${PARTNER}${MODE}-${STAGE} existed in remote, Taking backup now..."
+    BASE_BRANCH=$(git symbolic-ref --short HEAD)
+    echo "Creating backup branch with name" ${PARTNER}${MODE}-${STAGE}-date "+%Y%m%d-%H%M%S"
+    git checkout -b ${PARTNER}${MODE}-${STAGE}-date "+%Y%m%d-%H%M%S"
+    git push -u origin ${PARTNER}${MODE}-${STAGE}
+    echo "Deleting branch ${PARTNER}${MODE}-${STAGE}"
+    git push origin --delete ${PARTNER}${MODE}-${STAGE} &>/dev/null
+    git branch -D ${PARTNER}${MODE}-${STAGE} &>/dev/null
+    echo "Recreating branch ${PARTNER}${MODE}-${STAGE} from base branch ${BASE_BRANCH}"
+    git checkout -b ${PARTNER}${MODE}-${STAGE} ${BASE_BRANCH}
+    git push -u origin ${PARTNER}${MODE}-${STAGE}
 #end if we do not have the branch in remote create it
 fi
 
