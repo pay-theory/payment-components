67e246568daacbf1a9d87f47e07c4ffdca8e905c
EC2 Default User <your@email.com>
2022-02-10 21:33:38 +0000

    updating sdk to better indicate failure

--- pusher.sh
+++ pusher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git add .
-git commit -m 'updating websocket to pickup new role'
+git commit -m 'updating sdk to better indicate failure'
 git push
 git push origin premain:carpe-new-paytheorystudy
 git push origin premain:carpe-new-paytheory
