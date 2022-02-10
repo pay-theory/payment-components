9c9c3097515e109f2ae0cbb87c6c19d443b8e5e9
EC2 Default User <your@email.com>
2022-02-10 20:33:50 +0000

    updating websocket to pickup new role

---
+++ pusher.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git add .
+git commit -m 'updating websocket to pickup new role'
+git push
+git push origin premain:carpe-new-paytheorystudy
+git push origin premain:carpe-new-paytheory
+git push origin premain:carpe-old-paytheory
+git push origin premain:carpe-old-paytheorylab
+git push origin premain:carpe-old-paytheorystudy
