400e0d7a696d29ffecd052e7050679c839393030
EC2 Default User <your@email.com>
2022-02-11 13:38:35 +0000

    updating sdk url

--- pusher.sh
+++ pusher.sh
@@ -2,9 +2,9 @@
 git add .
 git commit -m 'updating sdk url'
 git push
-git push origin premain:carpe-new-paytheorystudy
-git push origin premain:carpe-new-paytheory
-git push origin premain:carpe-new-paytheorylab
-git push origin premain:carpe-old-paytheory
+# git push origin premain:carpe-new-paytheorystudy
+# git push origin premain:carpe-new-paytheory
+# git push origin premain:carpe-new-paytheorylab
+# git push origin premain:carpe-old-paytheory
 git push origin premain:carpe-old-paytheorylab
-git push origin premain:carpe-old-paytheorystudy
+# git push origin premain:carpe-old-paytheorystudy
