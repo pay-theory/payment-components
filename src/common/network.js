2f1785cfdcdfab98803d176988fcebd4a951636a
austinzani@gmail.com <austinzani@gmail.com>
2022-05-19 09:44:32 -0400

    updated shippingDetails to customerInfo

--- src/common/network.js
+++ src/common/network.js
@@ -164,7 +164,7 @@ const sendTransactingMessage = () => {
 
 export const generateInitialization = (handleInitialized, challengeOptions) => {
     return async(inputParameters) => {
-        let {amount, shippingDetails = {}, metadata = {}, confirmation = false} = inputParameters
+        let {amount, customerInfo = {}, metadata = {}, confirmation = false} = inputParameters
         let initialize = data.getInitialize()
         if (initialize !== 'init') {
             if (!Number.isInteger(amount) || amount < 1) {
@@ -174,7 +174,7 @@ export const generateInitialization = (handleInitialized, challengeOptions) => {
             data.setInitialize('init')
             const attested = await attestBrowser(challengeOptions)
 
-            await handleInitialized(amount, shippingDetails, metadata, confirmation)
+            await handleInitialized(amount, customerInfo, metadata, confirmation)
 
             if (attested.response) {
                 const transacting = data.getTransactingElement()
