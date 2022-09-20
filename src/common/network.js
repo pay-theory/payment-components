1826e618a03800706085511f1862863584b6b4a0
Aron Price <aron@paytheory.com>
2022-09-20 18:23:13 +0000

    url change

--- src/common/network.js
+++ src/common/network.js
@@ -40,7 +40,7 @@ export const transactionEndpoint = () => {
 }
 
 export const hostedFieldsEndpoint = () => {
-    return `https://${data.getEnvironment()}.tags.static.${data.getStage()}.com`
+    return `https://tags.app.${data.getEnvironment()}.${data.getStage()}.com`
 }
 
 export const generateTokenize = (cb) => {
