649530f6883200cab832311767e537aad006242f
austinzani@gmail.com <60404116+austinzani@users.noreply.github.com>
2022-12-14 14:21:28 -0500

    updating the logic to trigger cancel observer and barcode observer

--- src/components/pay-theory-checkout-button/index.js
+++ src/components/pay-theory-checkout-button/index.js
@@ -5,12 +5,10 @@ import {checkoutButtonField} from "../../common/data";
 class PayTheoryCheckoutButton extends HTMLElement {
     constructor() {
         super();
-        this._onCancel = () => {}
         this._onReady = () => {}
         this._onClick = () => {}
         this._onError = () => {}
         this._onSuccess = () => {}
-        this._onBarcode = () => {}
     }
 
     defineButton() {
@@ -27,7 +25,6 @@ class PayTheoryCheckoutButton extends HTMLElement {
         super.connectedCallback && super.connectedCallback();
 
         // Creating the listeners from the hosted checkout page
-        this._clearCancelListener = common.handleCheckoutMessage(common.checkoutCancelTypeMessage, this._onCancel)
         this._clearErrorListener = common.handleCheckoutMessage(common.checkoutErrorTypeMessage, this._onError)
         this._clearSuccessListener = common.handleCheckoutMessage(common.checkoutCompleteTypeMessage, this._onSuccess)
         this._clearBarcodeReceivedListener = common.handleCheckoutMessage(common.checkoutBarcodeReceivedTypeMessage, message => {
@@ -42,26 +39,16 @@ class PayTheoryCheckoutButton extends HTMLElement {
 
     disconnectedCallback() {
         super.disconnectedCallback && super.disconnectedCallback();
-        this._clearCancelListener()
         this._clearErrorListener()
         this._clearSuccessListener()
         this._clearReadyListener()
         this._clearClickListener()
         this._clearBarcodeReceivedListener()
-        this._clearBarcodeCompleteListener()
         common.removeButtonSuccess()
     }
 
     // Only want to allow event listeners to be set from outside the class
     // If they have been set before there should be a clear listener function so we want to clear it and reset the listener
-    set onCancel(cancelFunc) {
-        this._onCancel = cancelFunc
-        if(this._clearCancelListener) {
-            this._clearCancelListener()
-            this._clearCancelListener = common.handleCheckoutMessage(common.checkoutCancelTypeMessage, this._onCancel)
-        }
-    }
-
     set onReady(readyFunc) {
         this._onReady = readyFunc
         if(this._clearReadyListener) {
