e5bd265487dcd1a8ff439d99dd2f289c9011f5e7
JeffersonSchuler <40901906+JeffersonSchuler@users.noreply.github.com>
2023-04-10 18:11:17 -0400

    Added debug logging for Local Storage.

--- src/common/data.js
+++ src/common/data.js
@@ -152,6 +152,7 @@ export {
 
 const getLocalStorage = key => () => {
     try {
+        console.debug(`Getting Local Storage, ${key}, ${localStorage.getItem(key)}`)
         return localStorage.getItem(key)
     } catch (e) {
         window.postMessage({
@@ -166,6 +167,7 @@ const getLocalStorage = key => () => {
 
 const setLocalStorage = key => value => {
     try {
+        console.debug(`Setting Local Storage, ${key}, ${value}`)
         return localStorage.setItem(key, value)
     } catch (e) {
         window.postMessage({
