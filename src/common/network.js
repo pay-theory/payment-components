1728766de360f0b487692167c4cbe0db1023abe9
austinzani <60404116+austinzani@users.noreply.github.com>
2023-05-04 14:34:48 -0400

    fix for hardcoded interchange value

--- src/common/network.js
+++ src/common/network.js
@@ -201,10 +201,11 @@ export const generateInitialization = (handleInitialized, challengeOptions) => {
         // Adding line for backwards compatibility
         // TODO add some logging to SDK to see usage of deprecated variables and functions
         payorInfo = payorInfo || customerInfo || shippingDetails || {}
+        const newFeeMode = feeMode.toLowerCase() === "interchange" ? common.MERCHANT_FEE : feeMode
         let initialize = data.getInitialize()
         if (initialize !== 'init') {
             data.setInitialize('init')
-            const success = await handleInitialized(amount, payorInfo, payTheoryData, metadata, feeMode, confirmation)
+            const success = await handleInitialized(amount, payorInfo, payTheoryData, metadata, newFeeMode, confirmation)
             if (success) {
                 await handleAttestation(challengeOptions)
                 sendTransactingMessage()
