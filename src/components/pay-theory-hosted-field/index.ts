f24e633be936331d6a8629433f7a53d9d3a26129
austinzani <60404116+austinzani@users.noreply.github.com>
2023-05-05 15:11:58 -0400

    WIP: fixing connected callback of transacting field

--- src/components/pay-theory-hosted-field/index.ts
+++ src/components/pay-theory-hosted-field/index.ts
@@ -3,6 +3,7 @@ import common from '../../common'
 // @ts-ignore
 import DOMPurify from 'dompurify'
 import {elementTypes} from "../../common/data";
+import {handleError} from "../../common/message";
 
 export type styleObject = {
   default: object;
@@ -29,7 +30,6 @@ class PayTheoryHostedField extends HTMLElement {
   protected _placeholders: placeholderObject = {}
   constructor() {
     super()
-    this.defineFields = this.defineFields.bind(this)
     this.appendElement = this.appendElement.bind(this)
     this.setFields = this.setFields.bind(this)
     this.createToken = this.createToken.bind(this)
@@ -59,25 +59,18 @@ class PayTheoryHostedField extends HTMLElement {
     return encodeURI(encodedJson)
   }
 
-  defineFields(token: string) {
-    this.fields.forEach(field => {
-      const wrapper = `field-wrapper-${field?.replace(/\./, '-')}`
-      const wrapperElement = document.createElement('div')
-      wrapperElement.setAttribute('id', wrapper)
-      wrapperElement.setAttribute('class', 'field-wrapper')
-
-      const f = document.createElement('iframe')
-      f.setAttribute('src', `${common.hostedFieldsEndpoint}/${field}?token=${token}`)
-      f.setAttribute('frameBorder', '0')
-      f.setAttribute('name', `${field}-iframe`)
-      wrapperElement.appendChild(f)
-      this.appendElement(wrapperElement)
-    })
-  }
-
   appendElement(element: HTMLElement) {
     const container = document.getElementById(`pay-theory-${this.field}-hosted-field-container`)
     container?.appendChild(element)
+  }
+
+  setiFrameSrc() {
+    const token = this.createToken()
+    this.fields.forEach(field => {
+      const iframeUrl = `${common.hostedFieldsEndpoint}/${field}?token=${token}`
+      const iframe = document.getElementById(`${field}-iframe`)
+      iframe?.setAttribute('src', iframeUrl)
+    })
   }
 
   connectedCallback() {
@@ -85,8 +78,20 @@ class PayTheoryHostedField extends HTMLElement {
             <div id="pay-theory-${this.field}-hosted-field-container" class="pay-theory-field">
             </div>
         </div>`)
-    const token = this.createToken()
-    this.defineFields(token)
+    this.fields.forEach(field => {
+      const wrapper = `field-wrapper-${field?.replace(/\./, '-')}`
+      const wrapperElement = document.createElement('div')
+      wrapperElement.setAttribute('id', wrapper)
+      wrapperElement.setAttribute('class', 'field-wrapper')
+
+      const f = document.createElement('iframe')
+      f.setAttribute('frameBorder', '0')
+      f.setAttribute('name', `${field}-iframe`)
+      f.setAttribute('id', `${field}-iframe`)
+      wrapperElement.appendChild(f)
+      this.appendElement(wrapperElement)
+    })
+    this.setiFrameSrc()
   }
 
   disconnectedCallback() {
