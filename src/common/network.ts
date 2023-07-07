cd433d4710f2cbf277ef4fb999d64a6b4e241dab
austinzani <60404116+austinzani@users.noreply.github.com>
2023-07-07 12:15:19 -0400

    refactored sendTransactingMessage for duplicate code

--- src/common/network.ts
+++ src/common/network.ts
@@ -3,7 +3,7 @@ import {postMessageToHostedField} from './message'
 import PayTheoryHostedFieldTransactional from "../components/pay-theory-hosted-field-transactional";
 import {BillingInfo} from "./pay_theory_types";
 import {ErrorMessage, FieldsReadyMessage} from "./format";
-import {ACH_IFRAME, CARD_IFRAME, CASH_IFRAME} from "./data";
+import {ACH_IFRAME, CARD_IFRAME, CASH_IFRAME, ElementTypes} from "./data";
 
 export const getData = async(url: string, apiKey: string) => {
     const options: RequestInit = {
@@ -44,6 +44,17 @@ export const fetchPtToken = async(apiKey: string): Promise<{
     return false
 }
 
+const sendTransactingMessageToField = (field: ElementTypes, billingInfo: BillingInfo, channel?: MessagePort) => {
+    const iframeId = `${field}-iframe`
+    const iframe = document.getElementsByName(iframeId)[0]
+    if (iframe) {
+        postMessageToHostedField(`${field}-iframe`, {
+            type: "pt-static:transact",
+            element: field,
+            billingInfo
+        }, channel)
+    }
+}
 
 export const sendTransactingMessage = (transacting: PayTheoryHostedFieldTransactional, billingInfo: BillingInfo) => new Promise<ErrorMessage | FieldsReadyMessage>((resolve, reject) => {
     // Opening a new message channel, so we can await the response from the hosted field
@@ -63,26 +74,7 @@ export const sendTransactingMessage = (transacting: PayTheoryHostedFieldTransact
     const siblingFields = types.filter(field => ![CASH_IFRAME, ACH_IFRAME, CARD_IFRAME].includes(`${field}-iframe`))
     // Sending the message to the hosted field to transact first so that the channel port is saved in state
     // That will allow it to respond once it receives the message from the sibling fields
-    transactingField.forEach(field => {
-        const iframeId = `${field}-iframe`
-        const iframe = document.getElementsByName(iframeId)[0]
-        if (iframe) {
-            postMessageToHostedField(`${field}-iframe`, {
-                                type: "pt-static:transact",
-                                element: field,
-                                billingInfo
-                            }, channel.port2)
-        }
-    })
-    siblingFields.forEach(field => {
-        const iframeId = `${field}-iframe`
-        const iframe = document.getElementsByName(iframeId)[0]
-        if (iframe) {
-            postMessageToHostedField(`${field}-iframe`, {
-                                type: "pt-static:transact",
-                                element: field
-                            })
-        }
-    })
+    transactingField.forEach(field => sendTransactingMessageToField(field, billingInfo, channel.port2))
+    siblingFields.forEach(field => sendTransactingMessageToField(field, billingInfo))
 })
 
