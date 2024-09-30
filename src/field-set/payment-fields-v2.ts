38f2753d433580ecb067cd1bfaa1507e3798845b
austinzani <60404116+austinzani@users.noreply.github.com>
2024-09-30 17:25:50 -0400

    Update src/field-set/payment-fields-v2.ts
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/field-set/payment-fields-v2.ts
+++ src/field-set/payment-fields-v2.ts
@@ -48,7 +48,7 @@ interface ProcessedObject {
 // Used to generate a UUID for the session id
 const generateUUID = (): string => {
   // eslint-disable-next-line scanjs-rules/property_crypto
-  if (self.crypto && self.crypto.randomUUID) return self.crypto.randomUUID();
+  if (self.crypto?.randomUUID) return self.crypto.randomUUID();
   // If the browser doesn't support crypto.randomUUID, generate a random UUID
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0,
