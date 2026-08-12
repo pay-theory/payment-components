b8e3989bdbdcf3166278aa7fd96c921831af7296
Terrelle Tettey <terrelletettey@gmail.com>
2026-08-12 16:26:54 -0400

    Add iframeLoaded to field state type

--- src/common/pay_theory_types.ts
+++ src/common/pay_theory_types.ts
@@ -270,6 +270,7 @@ export interface FieldState {
   isFocused: boolean;
   isDirty: boolean;
   errorMessages: string[];
+  iframeLoaded?: boolean;
 }
 
 export type StateObject = Record<ElementTypes, FieldState> &
